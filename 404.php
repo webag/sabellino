a45fe6dcc52e117e7d68b7e29a868b2d3f74d33f
webag <andreiduffy@gmail.com>
2016-12-30 09:14:07 +0300

    Работал из дома, синхронизировал css. И сделал 404 страницу

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php include('header.php'); ?>
+
+
+<section class="s-404">
+	<div class="container">
+		<div class="s-404__left">404</div>
+		<div class="s-404__right">Ой, а&nbsp;такой страницы не&nbsp;существует.<br>Попробуйте начать <a href="/">с&nbsp;главной страницы</a></div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
