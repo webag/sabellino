358b6d9eddb6b2394b15734bacd87f32c8d9c288
Andrew <andreiduffy@gmail.com>
2017-06-08 20:34:50 +0300

    Работал из дома, добавлена страница команды

---
+++ team.php
@@ -0,0 +1,127 @@
+<?php include('header.php'); ?>
+
+<section class="page-title">
+	<h1 class="page-title__name">Наша команда</h1>
+</section>
+
+
+<section class="s-team">
+	<div class="container">
+
+		<div class="gendir">
+			<img src="/images/team/1.jpg" alt="Юшваева М.Н.">
+			<div class="gendir__content">
+				<span class="gendir__slogan">«Нацелены только на взаимовыгодное сотрудничество»</span>
+				<span class="gendir__descr">Генеральный директор SFG <small>Юшваева М.Н.</small></span>
+			</div>
+		</div>
+
+
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/2.jpg" alt="Никишина Юлия" class="team-hero__photo">
+				<span class="team-hero__name">Никишина Юлия</span>
+				<span class="team-hero__hwo">Менеджер по работе с клиентами</span>
+				<a href="tel:+7 (903) 755 40 96" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (903) 755 40 96</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/3.jpg" alt="Большова Евгения" class="team-hero__photo">
+				<span class="team-hero__name">Большова Евгения</span>
+				<span class="team-hero__hwo">Координатор отдела продаж</span>
+				<a href="tel:+7 (906) 753-97-39" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (906) 753-97-39</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/thumb.jpg" alt="Замаева Бэла" class="team-hero__photo">
+				<span class="team-hero__name">Замаева Бэла</span>
+				<span class="team-hero__hwo">Менеджер по работе с клиентами</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+
+
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/thumb.jpg" alt="Жамарина Юлия" class="team-hero__photo">
+				<span class="team-hero__name">Жамарина Юлия</span>
+				<span class="team-hero__hwo">Менеджер по работе с клиентами</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/4.jpg" alt="Завялец Виктория" class="team-hero__photo">
+				<span class="team-hero__name">Завялец Виктория</span>
+				<span class="team-hero__hwo">SMM менеджер</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/5.jpg" alt="Хайруллина Аэлита" class="team-hero__photo">
+				<span class="team-hero__name">Хайруллина Аэлита</span>
+				<span class="team-hero__hwo">Графический дизайнер</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+
+
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/6.jpg" alt="Банщиков Иван" class="team-hero__photo">
+				<span class="team-hero__name">Банщиков Иван</span>
+				<span class="team-hero__hwo">Бизнес-аналитик</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/7.jpg" alt="Луданик Надежда" class="team-hero__photo">
+				<span class="team-hero__name">Луданик Надежда</span>
+				<span class="team-hero__hwo">Экономист-аналитик</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/thumb.jpg" alt="Омельченко Геннадий" class="team-hero__photo">
+				<span class="team-hero__name">Омельченко Геннадий</span>
+				<span class="team-hero__hwo">Аналитик</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+
+
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/8.jpg" alt="Сизикова Юлия" class="team-hero__photo">
+				<span class="team-hero__name">Сизикова Юлия</span>
+				<span class="team-hero__hwo">Главный бухгалтер</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/thumb.jpg" alt="Куряч Оксана" class="team-hero__photo">
+				<span class="team-hero__name">Куряч Оксана</span>
+				<span class="team-hero__hwo">Заместитель главного бухгалтера</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+		<div class="grid-4 grid-6_m grid-12_s">
+			<div class="team-hero">
+				<img src="/images/team/9.jpg" alt="Федорова Марина" class="team-hero__photo">
+				<span class="team-hero__name">Федорова Марина</span>
+				<span class="team-hero__hwo">Офис-менеджер</span>
+				<a href="tel:+7 (499) 922-06-90" class="team-hero__phone"><img src="/images/team/phone.svg" alt=""> +7 (499) 922-06-90</a>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
