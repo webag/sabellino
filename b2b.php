f03dcacd85e3453e5f3440f77b2fe856333dee3b
Andrew <andreiduffy@gmail.com>
2017-10-25 17:40:42 +0300

    add b2b desktop

---
+++ b2b.php
@@ -0,0 +1,402 @@
+<?php include('header.php'); ?>
+
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800&amp;subset=greek" rel="stylesheet">
+
+<section class="s-b2b-head">
+	<div class="b2b-head">
+		<mark>Женская одежда, сумки и аксессуары оптом</mark>
+		<a href="#" data-ag="modal-b2b" class="btn btn--pink btn--big">Мне итересно</a>
+		<div class="b2b-head__descr">партии от <strong>10 000 руб</strong>, скидка <strong>5% на первый заказ</strong></div>
+	</div>
+</section>
+
+
+<section class="s-b2b-preims">
+	<p class="h3">Приемущества работы с нами</p>
+
+	<div class="b2b-preims-row">
+		<div class="b2b-preim">
+			<header>
+				<img src="/images/b2b/b2b-preim-1.svg" alt="11" width="78">
+			</header>
+			<div class="b2b-preim__title">Оборачиваимость выше рынка в&nbsp;2&nbsp;раза</div>
+			<p>Мы обеспечиваем вам высокий доход за&nbsp;счет рыночной наценки 200%</p>
+		</div>
+		<div class="b2b-preim">
+			<header>
+				<img src="/images/b2b/b2b-preim-2.svg" alt="11" width="58">
+			</header>
+			<div class="b2b-preim__title">Популярный бренд в&nbsp;молодежной среде</div>
+			<p>Высокая популярность торговой марки среди молодёжи<br>Широкая аудитория в&nbsp;Instagram</p>
+		</div>
+		<div class="b2b-preim">
+			<header>
+				<img src="/images/b2b/b2b-preim-3.svg" alt="11" width="76">
+			</header>
+			<div class="b2b-preim__title">Лидер рынка модной одежды, сумок и&nbsp;аксессуаров</div>
+			<p>Проверено на&nbsp;более чем 500 успешных партнерах</p>
+		</div>
+		<div class="b2b-preim">
+			<header>
+				<img src="/images/b2b/b2b-preim-5.svg" alt="11" width="80">
+			</header>
+			<div class="b2b-preim__title">Легко стать клиентом и&nbsp;открыть франшизу</div>
+			<p>Мы подберем вам модели популярные в&nbsp;вашем регионе</p>
+		</div>
+		<div class="b2b-preim">
+			<header>
+				<img src="/images/b2b/b2b-preim-1.svg" alt="11" width="78">
+			</header>
+			<div class="b2b-preim__title">Надежный поставщик</div>
+			<p>Мы обеспечиваем двойной контроль качества</p>
+		</div>
+	</div>
+</section>
+
+
+<section class="s-b2b-order">
+	<div class="b2b-order">
+		<div class="b2b-order__woman">
+			<img src="/images/b2b/order-1.jpg" alt="11">
+		</div>
+		<div class="b2b-order__block">
+			<p class="h3">Получить оптовый ПРАЙС-ЛИСТ</p>
+			<form class="ajax-form">
+				<div class="input-group">
+					<label>Ф.И.О. *</label>
+					<input type="text" name="user_name" data-req="true" data-label="Ф.И.О.">
+				</div>
+				<div class="input-group">
+					<label>E-mail *</label>
+					<input type="email" name="user_email" data-req="true" data-label="E-mail">
+				</div>
+				<div class="input-group">
+					<label>Телефон *</label>
+					<input type="tel" name="user_tel" data-req="true" data-label="Телефон">
+				</div>
+				<div class="registr-sec__note">
+					<label class="term_label">
+						<input type="checkbox" name="b2b-terms" checked>
+						Даю <a href="/about/documents/soglasie-na-obrabotku/" target="_blank" rel="nofollow">согласие</a> на обработку моих персональных данных, ознакомился с <a target="_blank" href="/about/documents/zashchita-personalnykh-dannykh/" rel="nofollow">положением о порядке хранения и защиты персональных данных</a> пользователей сайта
+					</label>
+				</div>
+				<input type="hidden" name="form_subject" value="Получить оптовый прайс-лист">
+				<button type="submit" class="btn btn--blue btn--big need_terms">Зарегистрироваться</button>
+			</form>
+			<div class="b2b-order__note">После регистрации вы получите Прайс-лист и доступ на сайте к оптовым ценам</div>
+		</div>
+		<div class="b2b-order__woman">
+			<img src="/images/b2b/order-2.jpg" alt="11">
+		</div>
+	</div>
+</section>
+
+
+<section class="s-b2b-shout">
+	<p class="h2">Sabellino - это модная и&nbsp;популярная одежда.</p>
+	<p class="h2">Большие продажи гарантированы!</p>
+</section>
+
+
+<section class="s-b2b-video">
+	<p class="h3">Видео о наших коллекциях</p>
+
+	<div class="b2b-video-row">
+		<div class="grid-4 grid-12_s">
+			<a href="https://www.youtube.com/embed/nN6qWmFIyGI?autoplay=1" class="b2b-video fancy-video fancybox.iframe">
+				<img src="/images/b2b/video-1.jpg" alt="1">
+				<div class="b2b-video__front">
+					<figure></figure>
+					<span>Коллекция "ххх"</span>
+				</div>
+			</a>
+		</div>
+		<div class="grid-4 grid-12_s">
+			<a href="https://www.youtube.com/embed/nN6qWmFIyGI?autoplay=1" class="b2b-video fancy-video fancybox.iframe">
+				<img src="/images/b2b/video-2.jpg" alt="2">
+				<div class="b2b-video__front">
+					<figure></figure>
+					<span>Коллекция "Angle bag"</span>
+				</div>
+			</a>
+		</div>
+		<div class="grid-4 grid-12_s">
+			<a href="https://www.youtube.com/embed/nN6qWmFIyGI?autoplay=1" class="b2b-video fancy-video fancybox.iframe">
+				<img src="/images/b2b/video-3.jpg" alt="3">
+				<div class="b2b-video__front">
+					<figure></figure>
+					<span>Коллекция "ххх"</span>
+				</div>
+			</a>
+		</div>
+	</div>
+
+	<a href="#" data-ag="modal-b2b" class="btn btn--pink btn--big">Убедили – присоединяюсь!</a>
+</section>
+
+
+<section class="s-b2b-shops">
+	<p class="h3">С нами работают крупнейшие магазины</p>
+
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-1.png" alt="1">
+	</div>
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-2.png" alt="1">
+	</div>
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-3.png" alt="1">
+	</div>
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-4.png" alt="1">
+	</div>
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-5.png" alt="1">
+	</div>
+	<div class="grid-2 grid-4_m">
+		<img src="/images/b2b/shop-6.png" alt="1">
+	</div>
+
+</section>
+
+
+<section class="s-b2b-geo">
+	<p class="h3">География продаж</p>
+
+	<div class="b2b-map-frame">
+		<div class="b2b-map-frame__text">Нам доверяют более 500 оптовых покупателей более чем в 100 городах России и странах СНГ</div>
+		<div id="b2b-map"></div>
+		<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBQfEu_8-pHG14T3d1VrAhof5J4UWKe7PE" async>
+		</script>
+	</div>
+
+</section>
+
+
+<section class="s-b2b-about">
+
+	<div class="grid-7 grid-12_m s-b2b-about__left">
+		<p class="h3">Видео о нашей компании</p>
+
+		<a href="https://www.youtube.com/embed/nN6qWmFIyGI?autoplay=1" class="b2b-video fancy-video fancybox.iframe">
+			<img src="/images/b2b/video-about.jpg" alt="1">
+			<div class="b2b-video__front">
+				<figure></figure>
+			</div>
+		</a>
+	</div>
+
+	<div class="grid-5 grid-12_m s-b2b-about__right">
+		<p class="h3">Отвечаем на ваши вопросы:</p>
+		<div class="b2b-about-block">
+			<strong class="b2b-about-block__q">Сейчас кризис, как быть уверенным, что товар будет продаваться?</strong>
+			<p class="b2b-about-block__ans">Нас знают и нам доверяют: мы работаем во всех регионах. Sabellino — топ бренд в молодежном сегменте в российском сегменте электронной коммерции. Почитайте открытые отзывы на wildberies.ru — лучшее доказательство Ваших продаж. Девушкам нравится дизайн и качество: 100% качество пошива и сырья.</p>
+		</div>
+		<div class="b2b-about-block">
+			<strong class="b2b-about-block__q">Хорошо, продаваться одежда будет, а доход приносить?</strong>
+			<p class="b2b-about-block__ans">Будет: маржинальность нашего товара в среднем около 200%!</p>
+		</div>
+		<div class="b2b-about-block">
+			<strong class="b2b-about-block__q">Если с товаром будет что-то не так, я потеряю деньги?</strong>
+			<p class="b2b-about-block__ans">Нет, мы поможем: если у Вас возникнут любые трудности, свяжитесь с нами - мы отзывчивые и поможем решить любые ситуации с нашим товаром</p>
+		</div>
+		<a href="#" data-ag="modal-b2b" class="btn btn--blue btn--big">Убедили – присоединяюсь!</a>
+	</div>
+
+</section>
+
+
+<section class="s-b2b-steps">
+	<p class="h3">Работать с Sabellino удобно и понятно</p>
+
+	<div class="b2b-steps-row">
+
+		<div class="b2b-step">
+			<header>
+				<span>1</span>
+				<img src="/images/b2b/step-1.png" alt="1">
+			</header>
+			<p>Вы оформляете заявку</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>2</span>
+				<img src="/images/b2b/step-2.png" alt="1">
+			</header>
+			<p>Через 30 минут с вами свяжется менеджер</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>3</span>
+				<img src="/images/b2b/step-3.png" alt="1">
+			</header>
+			<p>Вы рассказываете нашему эксперту о вашем ассортименте и клиентах</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>4</span>
+				<img src="/images/b2b/step-4.png" alt="1">
+			</header>
+			<p>Вы делаете заказ или мы сами формируем итоговую матрицу</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>5</span>
+				<img src="/images/b2b/step-5.png" alt="1">
+			</header>
+			<p>На следующий день мы предлагаем вам подходящую матрицу товара</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>6</span>
+				<img src="/images/b2b/step-6.png" alt="1">
+			</header>
+			<p>Вы получаете заказанный товар точно в срок</p>
+		</div>
+
+		<div class="b2b-step">
+			<header>
+				<span>7</span>
+				<img src="/images/b2b/step-7.png" alt="1">
+			</header>
+			<p>Если вам нужна будет помощь с товаром - наши менеджеры помогут вам</p>
+		</div>
+
+	</div>
+</section>
+
+
+<section class="s-b2b-order">
+	<div class="b2b-order">
+		<div class="b2b-order__woman">
+			<img src="/images/b2b/order-3.jpg" alt="11">
+		</div>
+		<div class="b2b-order__block">
+			<p class="h3">Получить оптовый ПРАЙС-ЛИСТ</p>
+			<form class="ajax-form">
+				<div class="input-group">
+					<label>Ф.И.О. *</label>
+					<input type="text" name="user_name" data-req="true" data-label="Ф.И.О.">
+				</div>
+				<div class="input-group">
+					<label>E-mail *</label>
+					<input type="email" name="user_email" data-req="true" data-label="E-mail">
+				</div>
+				<div class="input-group">
+					<label>Телефон *</label>
+					<input type="tel" name="user_tel" data-req="true" data-label="Телефон">
+				</div>
+				<div class="registr-sec__note">
+					<label class="term_label">
+						<input type="checkbox" name="b2b-terms" checked>
+						Даю <a href="/about/documents/soglasie-na-obrabotku/" target="_blank" rel="nofollow">согласие</a> на обработку моих персональных данных, ознакомился с <a target="_blank" href="/about/documents/zashchita-personalnykh-dannykh/" rel="nofollow">положением о порядке хранения и защиты персональных данных</a> пользователей сайта
+					</label>
+				</div>
+				<input type="hidden" name="form_subject" value="Получить оптовый прайс-лист">
+				<button type="submit" class="btn btn--blue btn--big need_terms">Зарегистрироваться</button>
+			</form>
+			<div class="b2b-order__note">После регистрации вы получите Прайс-лист и доступ на сайте к оптовым ценам</div>
+		</div>
+		<div class="b2b-order__woman">
+			<img src="/images/b2b/order-4.jpg" alt="11">
+		</div>
+	</div>
+</section>
+
+
+<div id="modal-b2b" class="agmodal">
+	<main>
+		<p class="agmodal__title">Зарегистрироваться</p>
+		<form class="ajax-form">
+			<div class="input-group">
+				<label>Ф.И.О. *</label>
+				<input type="text" name="user_name" data-req="true" data-label="Имя">
+			</div>
+			<div class="input-group">
+				<label>E-mail *</label>
+				<input type="email" name="user_mail" data-req="true"  data-label="E-mail">
+			</div>
+			<div class="input-group">
+				<label>Телефон *</label>
+				<input type="tel" name="user_phone" data-req="true" data-label="Телефон">
+			</div>
+			<input type="hidden" name="form_subject" value="b2b - регистрация">
+			<button type="submit" class="btn btn--blue btn--big need_terms">Отправить</button>
+			<div class="registr-sec__note">
+				<label class="term_label">
+					<input type="checkbox" name="b2b-terms" checked>
+					Даю <a href="/about/documents/soglasie-na-obrabotku/" target="_blank" rel="nofollow">согласие</a> на обработку моих персональных данных, ознакомился с <a target="_blank" href="/about/documents/zashchita-personalnykh-dannykh/" rel="nofollow">положением о порядке хранения и защиты персональных данных</a> пользователей сайта
+				</label>
+			</div>
+		</form>
+	</main>
+</div>
+
+
+<script>
+	$("input[name='b2b-terms']").change(function(){
+		var this_form = $(this).parents('form');
+		var this_btn = this_form.find('.need_terms');
+		if($(this).prop("checked"))
+			this_btn.removeAttr('disabled');
+		else
+			this_btn.attr('disabled','disabled');
+	});
+
+/**************************************************
+ Google Maps
+ ***************************************************/
+var locations = [
+	[55.746218, 37.519164],
+	[53.746318, 31.529164],
+	[54.746418, 32.539164],
+	[56.756218, 33.549164],
+	[57.766218, 34.559164]
+];
+
+$(document).ready(function(){
+	if ($('#b2b-map').length) {
+		google.maps.event.addDomListener(window, 'load', initMap);
+
+		function initMap() {
+
+			var mapOptions = {
+				zoom: 5,
+				scrollwheel: false,
+				mapTypeControl: false,
+				streetViewControl: false,
+				rotateControl: false,
+				fullscreenControl: false,
+				zoomControlOptions: {
+					position: google.maps.ControlPosition.LEFT_CENTER
+				},
+				center: new google.maps.LatLng(locations[0][0], locations[0][1])
+			};
+
+			var mapElement = document.getElementById('b2b-map');
+
+			var map = new google.maps.Map(mapElement, mapOptions);
+
+			var marker, i;
+			for (i = 0; i < locations.length; i++) {
+				marker = new google.maps.Marker({
+					position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+					map: map,
+					icon: '/images/b2b/pin.png'
+				});
+			}
+
+		}
+	}
+});
+/**************************************************
+ End Google Maps
+ ***************************************************/
+</script>
+
+<?php include('footer.php'); ?>
