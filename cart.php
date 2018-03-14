af34200edc0dca1593cf04cfd2879d200a3a7367
Andrew <andreiduffy@gmail.com>
2018-03-14 12:12:33 +0300

    add countdown to basket

--- cart.php
+++ cart.php
@@ -1,14 +1,28 @@
 <?php include('header.php'); ?>
 
+
 <section class="page-title">
 	<h1 class="page-title__name">Корзина</h1>
 </section>
 
-<section class="actions-section">
+<section class="actions-section actions-section--cart">
 	<a href="/catalog.php" class="back-link">Назад к списку товаров</a>
 	<div class="actions-section__right">
-		<a href="#" class="actions-section__1buy">Купить в 1 клик</a>
-		<a href="/checkout-opt.php" class="btn btn--blue btn--big">Оформить заказ</a>
+		<!--<a href="#" class="actions-section__1buy">Купить в 1 клик</a>-->
+		<div>
+			<a href="/checkout-opt.php" class="btn btn--blue btn--big">Оформить заказ</a>
+			<div class="cart-reserv">
+				<div class="cart-reserv__title">Заказ в резерве</div>
+				<div class="cart-reserv__subtitle"></div>
+				<div class="cart-reserv__counter">
+					<span class="cart-reserv__hours">00</span>
+					:
+					<span class="cart-reserv__minutes">00</span>
+					:
+					<span class="cart-reserv__seconds">00</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </section>
 
@@ -147,13 +161,110 @@
 </section>
 
 
-<section class="actions-section">
+<section class="actions-section actions-section--cart-bottom">
 	<a href="/catalog.php" class="back-link">Назад к списку товаров</a>
 	<div class="actions-section__right">
-		<a href="#" class="actions-section__1buy">Купить в 1 клик</a>
-		<a href="/checkout-rozn.php" class="btn btn--blue btn--big">Оформить заказ</a>
+		<!--<a href="#" class="actions-section__1buy">Купить в 1 клик</a>-->
+		<div>
+			<div class="cart-reserv">
+				<div class="cart-reserv__title">Заказ в резерве</div>
+				<div class="cart-reserv__subtitle"></div>
+				<div class="cart-reserv__counter">
+					<span class="cart-reserv__hours">00</span>
+					:
+					<span class="cart-reserv__minutes">00</span>
+					:
+					<span class="cart-reserv__seconds">00</span>
+				</div>
+			</div>
+			<a href="/checkout-rozn.php" class="btn btn--blue btn--big">Оформить заказ</a>
+		</div>
 	</div>
 </section>
 
+
+<script src="js/countdown.min.js" type="text/javascript" ></script>
+
+<script>
+	function setCookie(name, value, options) {
+		options = options || {};
+
+		var expires = options.expires;
+
+		if (typeof expires == "number" && expires) {
+			var d = new Date();
+			d.setTime(d.getTime() + expires * 1000);
+			expires = options.expires = d;
+		}
+		if (expires && expires.toUTCString) {
+			options.expires = expires.toUTCString();
+		}
+
+		value = encodeURIComponent(value);
+
+		var updatedCookie = name + "=" + value;
+
+		for (var propName in options) {
+			updatedCookie += "; " + propName;
+			var propValue = options[propName];
+			if (propValue !== true) {
+				updatedCookie += "=" + propValue;
+			}
+		}
+
+		document.cookie = updatedCookie;
+	}
+
+	function getCookie(name) {
+		var matches = document.cookie.match(new RegExp(
+			"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		));
+		return matches ? decodeURIComponent(matches[1]) : undefined;
+	}
+
+	var now = new Date();
+	var end = new Date();
+	var cookieTime = getCookie("reservtime");
+
+	if (cookieTime === undefined){
+		end = new Date(now.getFullYear(),now.getMonth(),now.getDate(),now.getHours(),now.getMinutes()+15,now.getSeconds());
+		setCookie("reservtime", end, {expires: 3600*60*60});
+	} else {
+		end = Date.parse(cookieTime);
+	}
+
+	function counter_update() {
+		var now = new Date();
+		var timespan = countdown(now, end, countdown.HOURS | countdown.MINUTES | countdown.SECONDS);
+
+		$('.cart-reserv__hours').text(timespan.hours);
+		$('.cart-reserv__minutes').text(timespan.minutes);
+		$('.cart-reserv__seconds').text(timespan.seconds);
+
+		var delta = (end - now);
+		if (delta < 0){
+			setReservEnd();
+			return false;
+		}
+
+		requestAnimationFrame(counter_update);
+	}
+
+	var delta = (end - now);
+	console.log(delta);
+	if (delta < 0){
+		setReservEnd();
+	} else {
+		counter_update();
+	}
+
+	function setReservEnd() {
+		$('.cart-reserv__hours').text('00');
+		$('.cart-reserv__minutes').text('00');
+		$('.cart-reserv__seconds').text('00');
+		$('.cart-reserv__title').text('Товар снят с резерва,');
+		$('.cart-reserv__subtitle').text('добавьте его еще раз в корзину.');
+	}
+</script>
 
 <?php include('footer.php'); ?>
